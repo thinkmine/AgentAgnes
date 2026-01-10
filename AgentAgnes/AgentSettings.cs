95bd83ffa3143fd41d9f9748c63d5f47a090ff6a
EdwardMoemeka <EdwardMoemeka@THINKMINE-001>
2026-01-09 21:38:26 -0500

    Different deployment

--- AgentAgnes/AgentSettings.cs
+++ AgentAgnes/AgentSettings.cs
@@ -8,7 +8,7 @@ public  class AgentSettings
 {
     public static string  ai_endpoint = "https://thinkmine-ai-demos.openai.azure.com/";
     public static string ai_access_key = "a57bf876732a4da7a0870c84373848f2";
-    public static string ai_deployment_name = "gpt-4o";
+    public static string ai_deployment_name = "gpt-4o2";
 }
 
 public class AgnesMessage
